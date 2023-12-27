175c4f3da62dd2dceb48533687ceedfca7f57abf
Ira Paniukova <ira.paniukova@serko.com>
2023-12-27 20:27:18 +1300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Button, Input, Stack, useTheme } from "@mui/material";
 
 function App() {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  // Load TODOs from local storage on app startup
+  useEffect(() => {
+    const storedList = localStorage.getItem("todos");
+    const storedTodos = storedList ? JSON.parse(storedList) : [];
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  // Update local storage whenever TODOs change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
+
+  const handleRemoveTodo = (index: number) => {
+    const newTodoList = todos.filter((_, i) => i !== index);
+    setTodos(newTodoList);
+  };
+  const pinkTheme = useTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>TO DO LIST</h1>
+      <div className="todo-input">
+        <Input
+          type="text"
+          placeholder="Add a new task"
+          inputProps={{
+            sx: {
+              color: pinkTheme.palette.secondary.light,
+              "::placeholder": {
+                color: pinkTheme.palette.secondary.light,
+              },
+            },
+          }}
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <Button variant="outlined" onClick={handleAddTodo}>
+          Add
+        </Button>
+      </div>
+      <Stack
+        direction="column"
+        alignItems="center"
+        spacing={2}
+        // justifyContent="space-between"
+        // width={300}
+      >
+        {todos.map((todo, index) => (
+          <Stack key={index} direction="row" spacing={1}>
+            <div>{todo}</div>
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </Stack>
+        ))}
+      </Stack>
+    </header>
   );
 }
 
